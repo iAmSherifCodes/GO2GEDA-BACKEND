bb4edccb851801d06d7702f360b568c839d8efc7
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-09-14 19:06:20 +0100

    Added source files

---
+++ src/main/java/com/go2geda/Go2GedaApplication.java
@@ -0,0 +1,13 @@
+package com.go2geda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Go2GedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Go2GedaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/go2geda/Go2GedaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go2geda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Go2GedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
